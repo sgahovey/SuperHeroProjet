c1773291d1a05137c1ee0bec393c1b9d086bb7d0
AHOVEY Saint-George <sgahovey@gmail.com>
2024-11-28 14:21:30 +0400

    Création du  Dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EquipeRepository;
+use App\Repository\MissionRepository;
+use App\Repository\SuperHeroRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(MissionRepository $missionRepo, SuperHeroRepository $heroRepo, EquipeRepository $equipeRepo): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+        $missionsEnCours = $missionRepo->findBy(['statut' => 'COMMENCÉE']);
+        $herosDisponibles = $heroRepo->findBy(['estDisponible' => true]);
+        $herosIndisponibles = $heroRepo->findBy(['estDisponible' => false]);
+        $herosTotal = count($herosDisponibles) + count($herosIndisponibles);
+    
+        $equipesActives = $equipeRepo->findBy(['estActive' => true]);
+    
+        return $this->render('/dashboard/index.html.twig', [
+            'missionsEnCours' => $missionsEnCours,
+            'herosDisponibles' => $herosDisponibles,
+            'herosIndisponibles' => $herosIndisponibles,
+            'herosTotal' => $herosTotal,
+            'equipesActives' => $equipesActives,
         ]);
     }
+    
+    
 }
